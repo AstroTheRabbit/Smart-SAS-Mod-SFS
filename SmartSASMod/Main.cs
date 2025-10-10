442754fe62a924b9764c2f9ea3c39ef426025eed
Peter Taylor <darthan184@gmail.com>
2025-10-10 10:25:27 +0100

    renamed to indicate this a forked version

--- SmartSASMod/Main.cs
+++ SmartSASMod/Main.cs
@@ -12,15 +12,15 @@ namespace SmartSASMod
 {
     public class Main : Mod
     {
-        public override string ModNameID => "smartsas";
-        public override string DisplayName => "Smart SAS";
-        public override string Author => "Astro The Rabbit";
+        public override string ModNameID => "smartsasdarth";
+        public override string DisplayName => "Smart SAS (Darthan Fork)";
+        public override string Author => "Astro The Rabbit, Darthan184";
         public override string MinimumGameVersionNecessary => "1.5.10.2";
-        public override string ModVersion => "1.7";
+        public override string ModVersion => "1.7-Darthan184-1";
         public override string Description => "Adds a variety of control options for the stability assist system (SAS).";
 
         public override Dictionary<string, string> Dependencies { get; } = new Dictionary<string, string> { { "UITools", "1.1.5" } };
-        public Dictionary<string, FilePath> UpdatableFiles => new Dictionary<string, FilePath>() { { "https://github.com/AstroTheRabbit/Smart-SAS-Mod-SFS/releases/latest/download/SmartSASMod.dll", new FolderPath(ModFolder).ExtendToFile("SmartSASMod.dll") } };
+//~         public Dictionary<string, FilePath> UpdatableFiles => new Dictionary<string, FilePath>() { { "https://github.com/AstroTheRabbit/Smart-SAS-Mod-SFS/releases/latest/download/SmartSASMod.dll", new FolderPath(ModFolder).ExtendToFile("SmartSASMod.dll") } };
 
         public static Mod mod;
         public static FolderPath modFolder;
@@ -44,7 +44,7 @@ namespace SmartSASMod
                     Assembly ANAISAssembly = Loader.main.GetLoadedMods().First(mod => mod.ModNameID == "ANAIS").GetType().Assembly;
                     Type velocityArrowPatch = ANAISAssembly.GetTypes().First(type => type.Name == "VelocityArrowDrawer_OnLocationChange_Patch");
                     ANAISTraverse = Traverse.Create(velocityArrowPatch);
-                } 
+                }
                 catch
                 {
                     Debug.Log("Smart SAS: ANAIS is not installed/active.");
